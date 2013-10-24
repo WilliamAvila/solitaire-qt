ab8b83971cb602951cfe385e36a10188db70965e
riku salkia <ext-riku.salkia@nokia.com>
2013-10-24 10:59:04 +0300

    renamed release_notes.txt -> README.md

---
+++ src/SolitaireView.cpp
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2011 Nokia Corporation.
+ */
+
+#include "SolitaireView.h"
+
+
+SolitaireView::SolitaireView(QGraphicsScene *scene, QWidget *parent)
+    : QGraphicsView(scene, parent)
+{
+}
+
+SolitaireView::~SolitaireView()
+{
+}
